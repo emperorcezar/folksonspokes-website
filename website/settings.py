548684709bc76eb24addbfa26dc4650c58f5beb3
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2012-05-30 12:00:44 -0500

    yay heroku

--- website/settings.py
+++ website/settings.py
@@ -169,3 +169,29 @@ TINYMCE_DEFAULT_CONFIG = {
     'theme': "simple",
     'relative_urls': False,
 }
+
+try:
+    if 'DATABASES' not in locals():
+        DATABASES = {}
+
+    if 'DATABASE_URL' in os.environ:
+        url = urlparse.urlparse(os.environ['DATABASE_URL'])
+
+        # Ensure default database exists.
+        DATABASES['default'] = DATABASES.get('default', {})
+
+        # Update with environment configuration.
+        DATABASES['default'].update({
+            'NAME': url.path[1:],
+            'USER': url.username,
+            'PASSWORD': url.password,
+            'HOST': url.hostname,
+            'PORT': url.port,
+        })
+        if url.scheme == 'postgres':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
+
+        if url.scheme == 'mysql':
+            DATABASES['default']['ENGINE'] = 'django.db.backends.mysql'
+except Exception:
+    print 'Unexpected error:', sys.exc_info()

501e9ae1d613581fc4a6ff3ac28d8fe8e1ac6426
Adam 'Cezar' Jenkins <emperorcezar@gmail.com>
2012-09-04 14:00:54 -0500

    Bad settings fix

--- website/settings.py
+++ website/settings.py
@@ -174,7 +174,7 @@ MEDIA_ROOT = '/%s/' % DEFAULT_S3_PATH
 MEDIA_URL = '//s3.amazonaws.com/%s/' % AWS_STORAGE_BUCKET_NAME
 STATIC_ROOT = "/%s/" % STATIC_S3_PATH
 STATIC_URL = '//s3.amazonaws.com/%s/static/' % AWS_STORAGE_BUCKET_NAME
-ADMIN_MEDIA_PREFIX = STATIC_URL + "admin"
+ADMIN_MEDIA_PREFIX = STATIC_URL + "admin/"
 
 TINYMCE_JS_URL = '/tiny_mce/tiny_mce_src.js'
 
